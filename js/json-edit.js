53257aa4c04fc20180e6c6387f6289715e56069c
Remote <develper@pardn.io>
2024-05-24 08:54:19 +0800

    202405240854

--- js/json-edit.js
+++ js/json-edit.js
@@ -60,6 +60,28 @@ document.addEventListener("DOMContentLoaded", function () {
             logout: function () {
                 page.data.is_guest = true;
             },
+            body_left_show: function (e) {
+                const dom = "section.body-left".$;
+
+                if (dom == null) {
+                    return;
+                };
+
+                const is_show = dom.$$class("show");
+                dom.$$class_(is_show, "show");
+            },
+            body_left_type: function (e) {
+                const dom = "section.body-left".$;
+
+                if (dom == null) {
+                    return;
+                };
+
+                console.log(dom.dataset)
+                const is_min = dom.dataset.min === "true";
+                dom.dataset.min = is_min ? "false" : "true";
+                _cookie("is_body_left_min", is_min ? "false" : "true")
+            },
             tab_show: function (e) {
                 const parent = this.$parent(0);
                 const is_show = parent.$$class("show") || parent.$sel("a[data-selected='true']");
